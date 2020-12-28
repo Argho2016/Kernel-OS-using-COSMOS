e283d2b9c71263930aef26090cd3e2b17d1ff3c7
Argho Das <aiub2016@yahoo.com>
2020-12-28 13:01:03 +0600

    Program Source Code

---
+++ CommandManager.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ArghOS.Accounts;
+using ArghOS.Programs;
+using ArghOS.Utils;
+
+using Cosmos.Core;
+
+
+namespace ArghOS.Programs
+{
+	public class CommandManager
+	{
+        public static void Init()
+        {
+            try
+            {
+            Commands:
+                ColorConsole.Write(ConsoleColor.Green, Reference.UserAccount.GetUsername());
+                ColorConsole.Write(ConsoleColor.Green, "@" + Reference.OSName);
+                ColorConsole.Write(ConsoleColor.White, ">");
+                ColorConsole.Write(ConsoleColor.Cyan,  Reference.CurrentDirectory);
+                ColorConsole.Write(ConsoleColor.White, Reference.DefaultAccessPrefix);
+
+                string input = Console.ReadLine();
+                string command = input.Split(" ")[0];
+
+                switch (command)
+                {
+                    // TODO Refactor all the commands
+                    case "cd":
+                        string newDirectory = input.Split(" ")[1];
+                        if (Directory.Exists(newDirectory))
+                        {
+                            if (newDirectory.Contains(Reference.RootPath))
+                            {
+                                Reference.CurrentDirectory = newDirectory;
+                            }
+                            else
+                            {
+                                Reference.CurrentDirectory += newDirectory;
+                            }
+                        }
+                        else
+                        {
+                            ColorConsole.WriteLine(ConsoleColor.Red, "The directory doesn't exist");
+                        }
+
+                        break;
+
+                    case "ls":
+                        string[] directories = Directory.GetDirectories(Reference.CurrentDirectory);
+                        for (int i = 0; i < directories.Length; i++)
+                        {
+                            ColorConsole.WriteLine(ConsoleColor.Cyan, directories[i]);
+                        }
+
+                        string[] files = Directory.GetFiles(Reference.CurrentDirectory);
+                        for (int i = 0; i < files.Length; i++)
+                        {
+                            ColorConsole.WriteLine(ConsoleColor.Cyan, files[i]);
+                        }
+
+                        break;
+
+                    case "mkdir":
+                        Directory.CreateDirectory(input.Split(" ")[1]);
+
+                        break;
+
+                    case "touch":
+                        File.Create(input.Split(" ")[1]);
+
+                        break;
+
+                    case "rm":
+                        string file = input.Split(" ")[1];
+                        if (File.Exists(file))
+                        {
+                            File.Delete(file);
+                        }
+                        else
+                        {
+                            ColorConsole.WriteLine(ConsoleColor.Red, "The specified file doesn't exist");
+                        }
+
+                        break;
+
+                    case "rmdir":
+                        string deleteDirectory = input.Split(" ")[1];
+                        if (Directory.Exists(deleteDirectory))
+                        {
+                            Directory.Delete(deleteDirectory, true);
+                        }
+                        else
+                        {
+                            ColorConsole.WriteLine(ConsoleColor.Red, "The specified directory doesn't exist");
+                        }
+
+                        break;
+
+                    case "cp":
+                        File.Copy(input.Split(" ")[1], input.Split(" ")[2], true);
+
+                        break;
+
+                    case "mv":
+                        string move = input.Split(" ")[1];
+                        if (Directory.Exists(move) || File.Exists(move))
+                        {
+                            Directory.Move(move, input.Split(" ")[2]);
+                        }
+                        else
+                        {
+                            ColorConsole.WriteLine(ConsoleColor.Red, "The source directory/file doesn't exist");
+                        }
+
+                        break;
+
+                    case "textpad":
+                        Textpad.Init(input.Split(" ")[1]);
+
+                        break;
+
+
+                    case "cls":
+                        Console.Clear();
+
+                        break;
+
+                    default:
+                        ColorConsole.WriteLine(ConsoleColor.Red, "Invalid command");
+
+                        break;
+                }
+
+                goto Commands;
+            }
+            catch (Exception e)
+            {
+                Global.mDebugger.Send("ERROR : " + e.Message + e.HResult.ToString());
+            }
+        }
+    }
+}
---
+++ Setup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using ArghOS.Accounts;
+using ArghOS.Programs;
+using ArghOS.Utils;
+
+namespace ArghOS.Programs
+{
+	public class Setup
+	{
+        public static void Init()
+        {
+            Console.Clear();
+
+            ColorConsole.WriteLine(ConsoleColor.Yellow, "Initiating setup...");
+            Console.WriteLine();
+
+            ColorConsole.WriteLine(ConsoleColor.Yellow, "Create your user account");
+            Console.WriteLine();
+
+            ColorConsole.Write(ConsoleColor.White, "Username: ");
+            string user = Console.ReadLine();
+
+            ColorConsole.Write(ConsoleColor.White, "Password: ");
+            string pass = Console.ReadLine();
+
+            ColorConsole.WriteLine(ConsoleColor.Yellow, "Creating user account...");
+            Reference.UserAccount = new Account(user, pass);
+            Reference.UserAccount.CreateAccount();
+            Console.WriteLine();
+
+            ColorConsole.WriteLine(ConsoleColor.Yellow, "Finished installation...");
+            File.WriteAllText(Reference.RootPath + "Installed.txt", "IsInstalled:True");
+            Console.WriteLine();
+
+            ColorConsole.Write(ConsoleColor.Yellow, "Press any key to restart...");
+            Console.ReadKey(true);
+
+            Power.Restart();
+        }
+
+        public static void Reset()
+        {
+            if (File.Exists(Reference.RootPath + "Installed.txt"))
+            {
+                ColorConsole.WriteLine(ConsoleColor.Yellow, "Finishing reset operation...");
+                File.Delete(Reference.RootPath + "Installed.txt");
+            }
+        }
+    }
+}
