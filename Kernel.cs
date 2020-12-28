2c6b0508be97c958c6deefa64537a93ed8db7e01
Argho Das <aiub2016@yahoo.com>
2020-12-28 13:02:19 +0600

    Kernel source Code

---
+++ Kernel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using System.IO;
+
+using ArghOS.Accounts;
+using ArghOS.Programs;
+using ArghOS.Utils;
+
+namespace ArghOS
+{
+	public class Kernel : Sys.Kernel
+	{
+        protected override void BeforeRun()
+        {
+            FileSystem.LoadFileSystem();
+
+            Power.ShowWelcomeMessage();
+
+            if (!File.Exists(Reference.RootPath + "Installed.txt"))
+            {
+                Setup.Init();
+            }
+
+            Power.Init();
+        }
+
+        protected override void Run()
+        {
+        Login:
+            ColorConsole.Write(ConsoleColor.White, "Username: ");
+            string user = Console.ReadLine();
+
+            ColorConsole.Write(ConsoleColor.White, "Password: ");
+            string pass = Console.ReadLine();
+
+            if (AccountManager.Exist(user) && AccountManager.GetPassword(user, true).Equals(pass))
+            {
+                Reference.UserAccount = new Account(user, pass);
+
+                goto Terminal;
+            }
+            else
+            {
+                ColorConsole.WriteLine(ConsoleColor.Red, "Login failed");
+
+                goto Login;
+            }
+
+        Terminal:
+            Power.Init();
+
+            while (true)
+            {
+                CommandManager.Init();
+            }
+        }
+
+        void EchoTest()
+        {
+            ColorConsole.Write(ConsoleColor.White, Reference.DefaultAccessPrefix);
+            var input = Console.ReadLine();
+            ColorConsole.WriteLine(ConsoleColor.White, input);
+        }
+    }
+}
